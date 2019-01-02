dad714798b89d40e268ff0981673b1f0024ed02b
kfivas <fiv15001@byui.edu>
2019-01-02 15:47:37 -0700

    Results function Updates

--- R/Results.R
+++ R/Results.R
@@ -1,14 +1,15 @@
 
 #' @title Combines the read_rep, calc, and excelfiles Functions.
 #' @param file_in = path to the .rep file you need. Entered as a string.
-#' @param weights = a variable that contains a path to a weights.csv file.
-#' @param file_out = a string name of what you want the function to output the file as.
+#' @param weights = a path to the .csv file with the needed weights. Entered as a string.
+#' @param file_out = a string name of what you want the function to output the file as. Entered as a string.
 #' @example results(file_in = "", weights = , file_out)
 #' @export 
 
 results <- function(file_in = NULL, weights = NULL, file_out = NULL){
   require(mosaic)
   require(tidyverse)
+  require(readr)
   require(fs)
   require(openxlsx)
   require(stringr)
@@ -16,6 +17,8 @@ results <- function(file_in = NULL, weights = NULL, file_out = NULL){
   
   dat <- quadrupole::read_rep(file_in)
   print("File digested")
+  
+  weights <- read_csv(weights)
   
   calculations <- calc(dat, weights)
   print("calculations done")
