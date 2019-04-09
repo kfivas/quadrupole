b97640501fe6e81a683f6bb27ef07adb3556e68c
kfivas <fiv15001@byui.edu>
2019-04-09 16:36:37 -0600

    Updates

--- R/Results.R
+++ R/Results.R
@@ -16,6 +16,7 @@ results <- function(file_in = NULL, weights = NULL, file_out = NULL){
   require(fs)
   require(openxlsx)
   require(stringr)
+  require(quadrupole)
   
   dat <- quadrupole::read_rep(file_in)
   print("File digested")
@@ -23,7 +24,7 @@ results <- function(file_in = NULL, weights = NULL, file_out = NULL){
   weights <- read_csv(weights)
   
   calculations <- quadrupole::calc(dat, weights)
-  print("calculations done")
+  print("Calculations done")
   
   excelFiles(calculations, mypath = file_out)
   print(paste0("Excel file created at ", file_out))
